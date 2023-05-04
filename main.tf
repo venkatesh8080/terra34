db5fea9bc47a8e2ac10f34f5642c1aac59322e7b
venkatesh8080 <129871533+venkatesh8080@users.noreply.github.com>
2023-05-04 09:46:01 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region = "us-east-1"
 access_key = "AKIAUXGPWNZ4MVNHC7CM"
 secret_key = "tXWyw4//X4MuY7/7GfIQX8ZfgYWP7kkOsHFJ6ldd"
 }
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "venky" {
 bucket = "vcube901439878"
 tags   = {
 Name = "dev"
