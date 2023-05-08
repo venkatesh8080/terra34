84126ff79dbef7c1c4f474cd5b781a82eb5d3331
venkatesh8080 <129871533+venkatesh8080@users.noreply.github.com>
2023-05-08 09:29:15 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIAUXGPWNZ4MVNHC7CM"
-secret_key = "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"
+access_key = "AKIAUXGPWNZ4EXZJTJ4Y"
+secret_key = "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"
 }
 resource "aws_s3_bucket" "venkatesh" {
 bucket = "vcube901439878"
